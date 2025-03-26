f40f15436458e4538e6c66b268141bede5b1e1cb
Tushar <tusharvarshney1810@gmail.com>
2025-03-26 16:50:14 +0530

    added render.yaml

--- index.js
+++ index.js
@@ -97,8 +97,10 @@ io.on('connection', (socket) => {
    });
 });
 
-server.listen(3002, () => {
-   console.log('Server is running on port 3002');
+const PORT = process.env.PORT || 3002; // Use Render's PORT environment variable or fallback to 3002
+
+server.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
 });
 
 
